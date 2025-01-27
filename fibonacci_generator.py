a5f5a9001cb1e2989d7cd8ed66a8a982f7fae9b2
jagriuth <173657547+jagruthi315@users.noreply.github.com>
2025-01-27 23:15:10 +0530

    Update fibonacci_generator.py
    commited some changes in the comments

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -16,5 +16,16 @@ try:
 
 except ValueError :
         print( " invalid input please enter an integer value")
-
-# here why is there a need to first store the function into a variable we could have just printed the function ryt  won't that be the same, well actually NO if we don't store the function in the variable before the print statement what will happen is in the for loop everytime function will be called from the start and not from the previous iteration why you ask because in that case we would be printing a function basically calling a function again and again in loop ..and the output will come 0 as many times you have given the range but now whats happening is we are calling a variable again and again which has stored the function of ours and if our function has values it also stores that so everytime we call it, it gives the stored value and the new value  also the variable should be created before the for loop starts..
+"""
+# why is there a need to first store the function into a variable we could have 
+just printed the function right..won't that be the same, well actually NO if we
+don't store the function in the variable before the print statement what will
+happen is in the for loop everytime function will be called from the start
+and not from the previous iteration why you ask because in that case we would
+be printing a function basically calling a function again and again in
+loop ..and the output will come 0 as many times you have given the range but 
+now whats happening here is we are calling a variable again and again which has stored
+the function of ours and if our function has values it also stores that so everytime
+we call it, it gives the stored value and the new value  also the variable should be 
+created before the for loop starts..
+"""
